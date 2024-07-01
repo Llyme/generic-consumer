846efea6ef11b1923f909e499417e9c389c35023
Michael Edmund Wong <infinitex777@gmail.com>
2024-07-01 13:39:11 +0800

    Improved BasicConsumer functionality.

--- generic_consumer/basic_consumer.py
+++ generic_consumer/basic_consumer.py
@@ -1,4 +1,5 @@
 from abc import ABC
+import json
 from typing import Any
 from .generic_consumer import GenericConsumer
 
@@ -10,22 +11,54 @@ class BasicConsumer(GenericConsumer, ABC):
 
     log = True
 
-    def _no_payloads(self) -> Any:
+    @classmethod
+    def _payload_is_json(cls):
+        """
+        If the payload should be converted into a dictionary.
+        """
+        return False
+
+    def _no_payloads(self):
+        """
+        Called if there are no available payloads.
+        """
         return False
 
     def _has_payloads(self, payloads: list):
+        """
+        Called if there is at least 1 payload.
+        """
         return True
 
+    def __try_json_payloads(self, payloads: list):
+        if payloads == None:
+            return None
+
+        if self._payload_is_json():
+            return payloads
+
+        result = []
+        ok = False
+
+        for payload in payloads:
+            try:
+                result.append(json.loads(payload))
+                ok = True
+            except:
+                pass
+
+        return result if ok else None
+
     def _run(self, payloads: list) -> Any:
+        payloads = self.__try_json_payloads(payloads)  # type: ignore
+
         if payloads == None:
             return self._no_payloads()
 
         count = len(payloads)
-
-        if count == 0:
-            return self._no_payloads()
+        queue_name = self.queue_name()
 
         if self.log:
-            print(f"Got {count} payload(s).")
+            print(f"Got {count} payload(s) from '{queue_name}'.")
 
         return self._has_payloads(payloads)
